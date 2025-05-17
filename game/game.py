756c3bca824bbc8e83b8c5264edf41b34d8c4318
Lorenzo Rota <lorenzo.rota@hotmail.com>
2025-05-17 17:00:35 +0200

    Fixed handle_input  and unittests

--- game/game.py
+++ game/game.py
@@ -1,5 +1,5 @@
 # from game_objects import *
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Tuple
 from enum import StrEnum
 from random import choices
 from webbrowser import Error
@@ -15,72 +15,80 @@ ACCEPTED_SYMBOLS = ["!", "@", "#", "$", "%"]
 CODE_LENGTH = 4
 MAX_TRIES = 10
 
+def _count_correct_positions(code: str, guess: str) -> Tuple[int, List[str | None], List[str | None]]:
+    result_pos = 0
+    code_copy = list(code)
+    guess_copy = list(guess)
+    for i in range(len(code_copy)):
+        if guess_copy[i] == code_copy[i]:
+            result_pos += 1
+            code_copy[i] = None
+            guess_copy[i] = None
+    return result_pos, code_copy, guess_copy
+
+
+def _count_correct_symbols(code_list: List[str | None], guess_list: List[str | None]) -> int:
+    result_sym = 0
+    for i in range(len(guess_list)):
+        if guess_list[i] is not None and guess_list[i] in code_list:
+            result_sym += 1
+            code_list[code_list.index(guess_list[i])] = None
+    return result_sym
+
+
+def _validate_code(input_str: str) -> Optional[ErrorMsg]:
+    if len(input_str) < CODE_LENGTH:
+        return ErrorMsg.CodeHasNotEnoughSigns
+    elif len(input_str) > CODE_LENGTH:
+        return ErrorMsg.CodeHasTooManySigns
+    elif not all(s in ACCEPTED_SYMBOLS for s in input_str):
+        return ErrorMsg.CodeHasInvalidSigns
+
+
 class Game:
     def __init__(self, code_string: str= None):
         self.nr_of_guesses_left: int = 0
         self.game_over: bool = False
-        self.board: List[str] = None
-        self.secret_code: List[str] = None
-
-    def _validate_code(self, input_str: str) -> Union[ErrorMsg | None]:
-        if len(input_str) < CODE_LENGTH:
-            return ErrorMsg.CodeHasNotEnoughSigns
-        elif len(input_str) > CODE_LENGTH:
-            return ErrorMsg.CodeHasTooManySigns
-        elif not all(s in ACCEPTED_SYMBOLS for s in input_str):
-            return ErrorMsg.CodeHasInvalidSigns
+        self._is_running: bool = False
+        self._board: List[str] = []
+        self._guesses: List[str] = []
+        self._results: List[str] = []
+        self._secret_code: str = ""
+        self.last_result: str = ""
 
     def start(self, code_string: str = None) -> Optional[ErrorMsg]:
         if code_string is None:
-            self.secret_code = choices(ACCEPTED_SYMBOLS, k=CODE_LENGTH)
-        elif self._validate_code(code_string) is None:
-                self.secret_code = [char for char in code_string]
+            self._secret_code = choices(ACCEPTED_SYMBOLS, k=CODE_LENGTH)
+            self._is_running = True
+        elif _validate_code(code_string) is None:
+            self._secret_code = [char for char in code_string]
+            self._is_running = True
         else:
             return ErrorMsg.InitialCodeInvalid
 
-    def handle_turn(self, input_str: str) -> Union[str | ErrorMsg]:
-        validation = self._validate_code(input_str)
-        # Return error message
-        if validation is not None:
-            return validation
+    def handle_turn(self, guess: str) -> Optional[ErrorMsg]:
+        if err := _validate_code(guess):
+            return err
 
-        result_pos = 0
-        result_sym = 0
-        # Compare given input with the secret code
-        input_str_list = [char for char in input_str]
-        correct_code = self.secret_code
+        pos_count, remaining_code, remaining_guess = _count_correct_positions(self._secret_code, guess)
+        sym_count = _count_correct_symbols(remaining_code, remaining_guess)
 
-        for i, (guessed_char, true_char) in enumerate(zip(input_str_list, correct_code)):
-            if guessed_char == true_char:
-                # correct position and correct symbol
-                result_pos += 1
-                correct_code[i] = "x"
-                input_str_list[i] = "x"
-
-        for i, guessed_char in enumerate(input_str_list):
-            for j, correct_char in enumerate(correct_code):
-                if i is j:
-                    continue
-
-                if guessed_char is correct_char:
-                    # correct symbol, incorrect pos
-                    correct_code[j] = "x"
-                    result_sym += 1
-                    break
-
-        return f"{input_str} | P{result_pos}, S{result_sym}"
+        self._guesses.append(guess)
+        self._results.append(result := f"P{pos_count}, S{sym_count}")
+        self.last_result = result
+        self._board.append(f"{guess} | {result}")
 
     def get_board_entries(self) -> List[str]:
-        return [""]
+        return self._board
 
     def is_running(self) -> bool:
-        return False
+        return self._is_running
 
     def get_guess_count(self) -> int:
         return self.nr_of_guesses_left
 
     def get_guesses(self) -> List[str]:
-        return self.board.get_guesses()
+        return self._board.get_guesses()
 
     def get_results(self) -> List[str]:
-        return self.board.get_results()
+        return self._board.get_results()
--- game/test/test_game.py
+++ game/test/test_game.py
@@ -1,167 +1,99 @@
+from webbrowser import Error
+
 import pytest
-from game.game import Game
+from game.game import Game, _validate_code
 from game.game import ErrorMsg
 
-# @pytest.fixture
-# def game():
-#     game = Game()
-#     return game
+@pytest.fixture
+def game():
+    return Game()
 
-def test_start_game_with_no_input():
+def test_start_game_with_random_code():
     for i in range(100):
         game = Game()
         assert game.start() is None
-        assert game._validate_code("".join(game.secret_code)) is None
+        assert _validate_code("".join(game._secret_code)) is None
 
-def test_start_game_with_input():
-    game = Game()
+def test_start_game_with_input(game):
     assert game.start("aaaaa") == ErrorMsg.InitialCodeInvalid
     assert game.start("!$$#") is None
 
-def test_is_running():
-    game = Game()
+def test_is_running(game):
     game.start()
     assert game.is_running()
 
-def test_is_code_valid():
-    game = Game()
-    assert game._validate_code("aaaa") is not None
-    assert game._validate_code("#####") is not None
-    assert game._validate_code("###") is not None
-    assert game._validate_code("!@@a") is not None
-    assert game._validate_code("!@#$") is None
-    assert game._validate_code("!!@@") is None
+def test_is_code_valid(game):
+    assert _validate_code("aaaa") == ErrorMsg.CodeHasInvalidSigns
+    assert _validate_code("#####") == ErrorMsg.CodeHasTooManySigns
+    assert _validate_code("###") == ErrorMsg.CodeHasNotEnoughSigns
+    assert _validate_code("!@@a") == ErrorMsg.CodeHasInvalidSigns
+    assert _validate_code("!@#$") is None
+    assert _validate_code("!!@@") is None
 
-def test_handle_valid_turn_symbols():
-    game = Game()
-    code = "@@##"
-    guess = "!!@!"
+@pytest.mark.parametrize("code, guess, expected_result",[
+    ("@@##", "!!@!", "P0, S1"),
+    ("@@##", "!!!@", "P0, S1"),
+    ("@@##", "!!@@", "P0, S2"),
+    ("!@#$", "$#@%", "P0, S3"),
+    ("!@#$", "$#@!", "P0, S4"),
+])
+def test_handle_valid_turn_symbols(game, code, guess, expected_result):
     game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P0, S1"
+    assert game.handle_turn(guess) is None
+    assert game.last_result == expected_result
 
-    game = Game()
-    code = "@@##"
-    guess = "!!!@"
+@pytest.mark.parametrize("code, guess, expected_result",[
+    ("!!@#", "$$$!", "P0, S1"),
+    ("!!@#", "$$!!", "P0, S2"),
+    ("!!!#", "$$$!", "P0, S1"),
+])
+def test_handle_valid_turn_correct_symbols_with_duplicate_symbols(game, code, guess, expected_result):
     game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P0, S1"
+    assert game.handle_turn(guess) is None
+    assert game.last_result == expected_result
 
-    game = Game()
-    code = "@@##"
-    guess = "!!@@"
+@pytest.mark.parametrize("code, guess, expected_result",[
+    ("!@#$", "!%%%", "P1, S0"),
+    ("!@#$", "!%%$", "P2, S0"),
+    ("!@#$", "!@#%", "P3, S0"),
+    ("!@#$", "!@#$", "P4, S0"),
+])
+def test_handle_valid_turn_positions_and_symbols(game, code, guess, expected_result):
     game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P0, S2"
+    assert game.handle_turn(guess) is None
+    assert game.last_result == expected_result
 
-    game = Game()
-    code = "!@#$"
-    guess = "$#@%"
+@pytest.mark.parametrize("code, guess, expected_result",[
+    ("!!@@", "!!!%", "P2, S0"),
+    ("!@@!", "!%!!", "P2, S0"),
+])
+def test_handle_valid_turn_positions_and_symbols_with_duplicate_symbols(game, code, guess, expected_result):
     game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P0, S3"
+    assert game.handle_turn(guess) is None
+    assert game.last_result == expected_result
 
-    game = Game()
-    code = "!@#$"
-    guess = "$#@!"
+@pytest.mark.parametrize("code, guess, expected_result",[
+    ("@@##", "@#$$", "P1, S1"),
+    ("@@%$", "@@$%", "P2, S2"),
+])
+def test_handle_valid_turn_both_p_and_s_non_zero(game, code, guess, expected_result):
     game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P0, S4"
+    assert game.handle_turn(guess) is None
+    assert game.last_result == expected_result
 
-def test_handle_valid_turn_correct_symbols_with_duplicate_symbols():
-    game = Game()
-    code = "!!@#"
-    guess = "$$$!"
+@pytest.mark.parametrize("code, guess, expected_error_msg",[
+    ("@@##", "@#!@#", ErrorMsg.CodeHasTooManySigns),
+    ("@@##", "@#!", ErrorMsg.CodeHasNotEnoughSigns),
+    ("@@##", "@#!a", ErrorMsg.CodeHasInvalidSigns),
+])
+def test_handle_invalid_turn(game, code, guess, expected_error_msg):
     game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P0, S1"
+    assert game.handle_turn(guess) == expected_error_msg
 
-    game = Game()
-    code = "!!@#"
-    guess = "$$!!"
-    game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P0, S2"
-
-    game = Game()
-    code = "!!!#"
-    guess = "$$$!"
-    game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P0, S1"
-
-def test_handle_valid_turn_positions_and_symbols():
-    game = Game()
-    code = "!@#$"
-    guess = "!%%%"
-    game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P1, S0"
-
-    game = Game()
-    code = "!@#$"
-    guess = "!%%$"
-    game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P2, S0"
-
-    game = Game()
-    code = "!@#$"
-    guess = "!@#%"
-    game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P3, S0"
-
-    game = Game()
-    code = "!@#$"
-    guess = "!@#$"
-    game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P4, S0"
-
-def test_handle_valid_turn_positions_and_symbols_with_duplicate_symbols():
-    game = Game()
-    code = "!!@@"
-    guess = "!!!%"
-    game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P2, S0"
-
-    game = Game()
-    code = "!@@!"
-    guess = "!%!!"
-    game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P2, S0"
-
-def test_handle_valid_turn_both_p_and_s_non_zero():
-    game = Game()
-    code = "@@##"
-    guess = "@#$$"
-    game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P1, S1"
-
-    game = Game()
-    code = "@@%$"
-    guess = "@@$%"
-    game.start(code)
-    out = game.handle_turn(guess)
-    assert out == f"{guess} | P2, S2"
-
-def test_handle_invalid_turn():
-    game = Game()
-    game.start("@@##")
-    out = game.handle_turn("@#!@#")
-    assert out == ErrorMsg.CodeHasTooManySigns
-
-    game = Game()
-    game.start("@@##")
-    out = game.handle_turn("@#!")
-    assert out == ErrorMsg.CodeHasNotEnoughSigns
-
-    game = Game()
-    game.start("@@##")
-    out = game.handle_turn("@#!a")
-    assert out == ErrorMsg.CodeHasInvalidSigns
-
+# def test_board(game):
+#     game.start("!@#$")
+#     assert game.handle_turn("!%%%") is None
+#     assert game.handle_turn("!%%$") is None
+#     assert game.handle_turn("!@#%") is None
+#     assert game.handle_turn("!@#$") is None
+#     print(game._board)
